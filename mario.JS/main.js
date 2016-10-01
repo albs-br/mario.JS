fe5d27bb6520340d715c2f846f5c41e958772326
albs-br <albs_br@yahoo.com.br>
2016-10-01 17:28:53 -0300

    Refactoring

--- mario.JS/main.js
+++ mario.JS/main.js
@@ -7,28 +7,6 @@ c.width = NES_HORIZONTAL_RES;
 c.height = NES_VERTICAL_RES;
 var ctx = c.getContext("2d");
 
-
-
-marioSpritesEnum = {
-	STANDING: 0,
-	WALKING1: 1,
-	WALKING2: 2,
-	WALKING3: 3,
-	JUMPING: 5,
-	CROUCHING: 6
-}
-
-var mario = {
-	X: (NES_HORIZONTAL_RES/2) - (16/2), //middle of screen
-	Y: 16*12,
-	lookingTo: 'right', // 'left' or 'right'
-	spriteNumber: marioSpritesEnum.STANDING,
-	state: 0, // 0: Mario, 1: Super Mario, 2: Luigi, 3: Super Luigi
-	horizontalSpeed: 0,
-	jumpHeight: 0,
-	falling: false
-}
-
 var gameLoop = function() {
 	
 	clearScreen();
@@ -67,7 +45,7 @@ var gameLoop = function() {
 			mario.jumpHeight = 0;
 		}
 	}
-	else if (mario.jumpHeight > 0 && !gamePad.A) { // released the jump button during a jump
+	else if (mario.isJumping() && !gamePad.A) { // released the jump button during a jump
 		mario.falling = true;
 	}
 	else if (gamePad.A) {
---
+++ mario.JS/mario.js
@@ -0,0 +1,23 @@
+﻿marioSpritesEnum = {
+	STANDING: 0,
+	WALKING1: 1,
+	WALKING2: 2,
+	WALKING3: 3,
+	JUMPING: 5,
+	CROUCHING: 6
+}
+
+var mario = {
+	X: (NES_HORIZONTAL_RES / 2) - (16 / 2), //middle of screen
+	Y: 16 * 12,
+	lookingTo: 'right', // 'left' or 'right'
+	spriteNumber: marioSpritesEnum.STANDING,
+	state: 0, // 0: Mario, 1: Super Mario, 2: Luigi, 3: Super Luigi
+	horizontalSpeed: 0,
+	jumpHeight: 0,
+	falling: false,
+
+	isJumping: function () {
+		return (this.jumpHeight > 0);
+	}
+}
